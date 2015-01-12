6c6db1f0d2748d916c73f9a4ec06866c2da7a6fc
Andrew Danger Lyon <orthecreedence@gmail.com>
2015-01-11 23:13:14 -0800

    removing unneeded hadler-bind wraping make-accessor call

--- accessors.lisp
+++ accessors.lisp
@@ -25,9 +25,7 @@
                    (or (string= "-S" (subseq str (- (length str) 2)))
                        (string= "-T" (subseq str (- (length str) 2)))))
           (push
-            `(handler-bind
-                 ((error (lambda (e) e)))
-               (progn (make-accessors ,sym)))
+            `(progn (make-accessors ,sym))
             makers))))
     `(progn ,@makers)))
 
