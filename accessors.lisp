bae74f2ac1df3cc1b1d6bfc2a6363a0a751aab37
Andrew Danger Lyon <orthecreedence@gmail.com>
2015-01-11 23:12:28 -0800

    fixing accessors, adding addr structs, etc. working nicely

--- accessors.lisp
+++ accessors.lisp
@@ -5,14 +5,17 @@
       (ignore-errors (foreign-slot-names `(:struct ,(intern (string c-struct) :libuv))))
     (when error
       (return-from make-accessors))
-    `(progn
-       ,@(loop for slot-name in slots
-               for accessor-name = (intern (concatenate 'string (symbol-name c-struct)
-                                                        "-"
-                                                        (symbol-name slot-name)))
-               append (list `(defmacro ,accessor-name (ptr)
-                               (list 'foreign-slot-value ptr ''(:struct ,(intern (string c-struct) :libuv)) '',slot-name))
-                            `(export ',accessor-name :libuv.accessors))))))
+    (let ((_ptr (gensym "ptr")))
+      `(progn
+         ,@(loop for slot-name in slots
+                 for accessor-name = (intern (concatenate 'string (symbol-name c-struct)
+                                                          "-"
+                                                          (symbol-name slot-name)))
+                 append (list `(defmacro ,accessor-name (,_ptr)
+                                 (list 'foreign-slot-value ,_ptr '(:struct ,(intern (string c-struct) :libuv)) ',slot-name))
+                              `(import ',slot-name :libuv)
+                              `(export ',slot-name :libuv)
+                              `(export ',accessor-name :libuv.accessors)))))))
 
 (defmacro do-class-symbols ()
   (let ((makers nil))
@@ -22,13 +25,19 @@
                    (or (string= "-S" (subseq str (- (length str) 2)))
                        (string= "-T" (subseq str (- (length str) 2)))))
           (push
-            `(handler-case
-               (progn (make-accessors ,sym))
-               ;; probably not a class, skip it
-               (error () nil))
+            `(handler-bind
+                 ((error (lambda (e) e)))
+               (progn (make-accessors ,sym)))
             makers))))
     `(progn ,@makers)))
 
 (eval-when (:compile-toplevel :load-toplevel)
-  (do-class-symbols))
+  (do-class-symbols)
+
+  (make-accessors in-addr)
+  (make-accessors in6-addr)
+  (make-accessors sockaddr)
+  (make-accessors sockaddr-in)
+  (make-accessors sockaddr-in6)
+  (make-accessors addrinfo))
 
